dff93036d39feaa75756fedfffb7acddacc0c691
Heitor <115489905+Astr0ns@users.noreply.github.com>
2024-01-03 15:38:29 +0000

    finidh

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ hiddenAlinhar.forEach((el) => observer.observe(el));
 
 const hiddenConsltorio = document.querySelectorAll('.about-consultorio');
 hiddenConsltorio.forEach((el) => observer.observe(el));
+
+
+document.getElementById('botaoContato').addEventListener('click', function (e) {
+    e.preventDefault();
+    scrollToElement('.agenda-consulta');
+});
+
+function scrollToElement(selector) {
+    var element = document.querySelector(selector);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
+
+document.getElementById('botaoConsultorio').addEventListener('click', function (e) {
+    e.preventDefault();
+    scrollToElement('.about-consultorio');
+});
+
+function scrollToElement(selector) {
+    var element = document.querySelector(selector);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
+}
